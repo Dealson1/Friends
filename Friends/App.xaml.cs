0aef25160297a5bea7e785b80ba53ae4531de38c
opilane <opilane@HP-CZC2349HTC>
2023-04-17 14:50:03 +0300

    Add project files.

---
+++ Friends/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Friends.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Friends
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new FriendsListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
